1beaa9dc05c111282533a9695d88c042d4f93808
Tiago Azevedo <up202108699@edu.fe.up.pt>
2025-01-04 11:31:26 +0000

    Change from tubegeometry to mesh of triangles

--- tp3/object/MyTrack .js
+++ tp3/object/MyTrack .js
@@ -12,13 +12,13 @@ class MyTrack extends THREE.Object3D {
         this.app = app;
         this.points = points;
         this.segments = 300;
-        this.width = 2;
+        this.width = 20;
         this.textureRepeat = 10;
         this.showWireframe = false;
         this.showMesh = true;
         this.showLine = true;
         this.closedCurve = true;
-        this.path = new THREE.CatmullRomCurve3(points);
+        this.path = new THREE.CatmullRomCurve3(points, true);
         this.object = null;
 
         this.widthS = 5;
@@ -27,11 +27,12 @@ class MyTrack extends THREE.Object3D {
         // define texture
         const loader = new THREE.TextureLoader();
         const texture = loader.load("./image/road.jpg");
-        texture.wrapS = THREE.RepeatWrapping;
+        texture.rotation = Math.PI / 2;
+        texture.center.set(0.5, 0.5);   
 
         // define material
         this.material = new THREE.MeshBasicMaterial({ map: texture });
-        this.material.map.repeat.set(this.textureRepeat, 3);
+        this.material.map.repeat.set(this.textureRepeat, 1);
         this.material.map.wrapS = THREE.RepeatWrapping;
         this.material.map.wrapT = THREE.RepeatWrapping;
         this.lineMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
@@ -62,9 +63,9 @@ class MyTrack extends THREE.Object3D {
         this.selectorA.name = "side_1";
         this.selectorB.name = "side_2";
 
-        const posX = -this.points[0].x * this.widthS;
-        const posY = this.points[0].y * this.heightS;
-        const posZ = this.points[0].z * this.widthS;
+        const posX = this.points[0].x //* this.widthS;
+        const posY = this.points[0].y  * this.heightS;
+        const posZ = this.points[0].z //* this.widthS;
 
         this.selectorA.position.set(posX - 5, posY + 1, posZ);
         this.selectorB.position.set(posX + 5, posY + 1, posZ);
@@ -78,31 +79,121 @@ class MyTrack extends THREE.Object3D {
      */
     buildObject() {
         // define geometry
-        const geometry = new THREE.TubeGeometry(
-            this.path,
-            this.segments,
-            this.width,
-            3,
-            this.closedCurve
-        );
+        //const geometry = new THREE.TubeGeometry(
+        //    this.path,
+        //    this.segments,
+        //    this.width,
+        //    3,
+        //    this.closedCurve
+        //);
 
-        // define mesh, wireframe and line
-        this.mesh = new THREE.Mesh(geometry, this.material);
-        this.wireframe = new THREE.Mesh(geometry, this.wireframeMaterial);
-        let points = this.path.getPoints(this.segments);
-        let bGeometry = new THREE.BufferGeometry().setFromPoints(points);
-        this.line = new THREE.Line(bGeometry, this.lineMaterial);
+        //// define mesh, wireframe and line
+        //this.mesh = new THREE.Mesh(geometry, this.material);
+        //this.wireframe = new THREE.Mesh(geometry, this.wireframeMaterial);
+        //let points = this.path.getPoints(this.segments);
+        //let bGeometry = new THREE.BufferGeometry().setFromPoints(points);
+        //this.line = new THREE.Line(bGeometry, this.lineMaterial);
 
-        // build object
+        //// build object
         this.object = new THREE.Group();
+
+        //this.mesh.visible = this.showMesh;
+        //this.wireframe.visible = this.showWireframe;
+        //this.line.visible = this.showLine;
+        //this.object.add(this.mesh);
+        //this.object.add(this.wireframe);
+        //this.object.add(this.line);
+        //this.object.rotateZ(Math.PI);
+        //this.object.scale.set(this.widthS, this.heightS, this.widthS);
+
+
+        //Track
+        const L = this.width; 
+        const up = new THREE.Vector3(0, 1, 0); 
+        const leftPoints = [];
+        const rightPoints = [];
+
+        const points = this.path.getPoints(this.segments);
+
+        for (let i = 0; i < points.length; i++) {
+            const t = i / (points.length - 1);
+            const tangent = this.path.getTangent(t).normalize();
+            const normal = new THREE.Vector3().crossVectors(up, tangent).normalize();
+            const left = points[i].clone().add(normal.clone().multiplyScalar(L / 2));
+            const right = points[i].clone().add(normal.clone().multiplyScalar(-L / 2));
+        
+            leftPoints.push(left);
+            rightPoints.push(right);
+        }
+        
+        const vertices = [];
+        const indices = [];
+        const uvs = [];
+
+        let totalLength = 0;                      
+        let lengths = [0];                        
+        for (let i = 1; i < points.length; i++) {
+            const dist = points[i - 1].distanceTo(points[i]);
+            totalLength += dist;                   
+            lengths.push(totalLength);             
+        }
+
+        for (let i = 0; i < leftPoints.length - 1; i++) {
+            vertices.push(
+                leftPoints[i].x, leftPoints[i].y, leftPoints[i].z,
+                points[i].x, points[i].y, points[i].z,
+                rightPoints[i].x, rightPoints[i].y, rightPoints[i].z,
+                leftPoints[i + 1].x, leftPoints[i + 1].y, leftPoints[i + 1].z,
+                points[i + 1].x, points[i + 1].y, points[i + 1].z,
+                rightPoints[i + 1].x, rightPoints[i + 1].y, rightPoints[i + 1].z
+            );
+        
+            const baseIndex = i *6; //* 4;
+
+            //indices.push(baseIndex, baseIndex + 1, baseIndex + 2);
+            //indices.push(baseIndex + 1, baseIndex + 3, baseIndex + 2);
+
+            //left
+            indices.push(baseIndex, baseIndex + 1, baseIndex + 3);
+            indices.push(baseIndex + 1, baseIndex + 4, baseIndex + 3);
+
+            //right
+            indices.push(baseIndex + 1, baseIndex + 2, baseIndex + 4);
+            indices.push(baseIndex + 2, baseIndex + 5, baseIndex + 4);
+
+            const v1 = lengths[i] / totalLength;
+            const v2 = lengths[i + 1] / totalLength;
+
+            uvs.push(
+                0, v1,
+                0.5, v1,
+                1, v1,
+                0, v2,
+                0.5, v2,
+                1, v2
+            );
+        }
+        
+        const geometry = new THREE.BufferGeometry();
+        geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+        geometry.setIndex(indices);
+        geometry.computeVertexNormals();
+        geometry.setAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2));
+        
+        this.mesh = new THREE.Mesh(geometry, this.material);
+        this.mesh.translateY(0.1)
+        this.wireframe = new THREE.Mesh(geometry, this.wireframeMaterial);
+
+        const bGeometry = new THREE.BufferGeometry().setFromPoints( points );
+        this.line = new THREE.Line( bGeometry, this.lineMaterial);
+        this.line.translateY(0.2)
+
         this.mesh.visible = this.showMesh;
         this.wireframe.visible = this.showWireframe;
         this.line.visible = this.showLine;
         this.object.add(this.mesh);
         this.object.add(this.wireframe);
         this.object.add(this.line);
-        this.object.rotateZ(Math.PI);
-        this.object.scale.set(this.widthS, this.heightS, this.widthS);
     }
 
     /**
@@ -132,7 +223,7 @@ class MyTrack extends THREE.Object3D {
      * @param {number} value - repeat value in S (or U) provided by user
      */
     updateTextureRepeat(value) {
-        this.material.map.repeat.set(value, 3);
+        this.material.map.repeat.set(value, 1);
     }
 
     /**
